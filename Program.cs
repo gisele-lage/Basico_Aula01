5a6b32f26b582cd4e1263962bc8a606816f2a776
Gisele Lage <giselelage24@gmail.com>
2025-04-11 20:14:22 -0300

    Criação de variáveis, entrada e saída de dados

--- Program.cs
+++ Program.cs
@@ -12,6 +12,31 @@ namespace Basico_Aula01
         {
             //Saída de dados
             Console.WriteLine("Ola, Mundo!");
+            
+            /*------------------------------------------------*/
+
+            // Criando variáveis
+
+            int idade = 20; // Declarando e inicializando a variável
+            string nome; // Apenas declarando a variável
+
+            float altura = 1.55f; // float usa f no final
+            double altura_2 = 1.55; 
+
+
+
+            /*-------------------------------------------------*/
+
+            // Entrada de dados
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine(); // Lê a entrada de dados
+
+            Console.WriteLine("O seu nome é: {0}", nome); // Exibe o nome na tela
+            Console.WriteLine("A sua idade é: {0}", idade);
+            Console.WriteLine("A sua altura é: {0}", altura);
+            Console.WriteLine("A altura usando o Double é: {0}", altura_2);
+
+
         }
     }
 }
